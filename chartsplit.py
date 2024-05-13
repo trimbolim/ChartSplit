acb58c61f0b83b6d08d7317faaa131d7d1742372
Matthew Trimboli <matthewtrimboli@Matthews-MacBook-Pro.local>
2024-05-13 14:25:09 -0400

    made it work

--- chartsplit.py
+++ chartsplit.py
@@ -3,38 +3,28 @@
 import yaml
 import argparse
 import pycpdflib
+import logging
 
 parser = argparse.ArgumentParser(description='Split Master PDFs into parts')
 parser.add_argument('file_to_split',  type=str, 
                     help='Master PDF to be split.')
 parser.add_argument('--map', help='path to yaml file with parts map', type=str, required=True)
 parser.add_argument('--outfile', help='filename for output files', type=str, required=True)
+parser.add_argument('--push', help='push files into Swing Shift folders', action='store_true' )
+
+SWING_SHIFT_PARTS_DIR = "/Users/matthewtrimboli/Google Drive/My Drive/Swing Shift/Charts/Parts"
 
 args = parser.parse_args()
 
-#print (args)
-#print (args.outfile)
-
-
 # /Users/matthewtrimboli/drums.pdf
 pycpdflib.loadDLL("/usr/local/lib/libpycpdf.so")
-bigpdf = pycpdflib.fromFile('/Users/matthewtrimboli/drums.pdf', '')
+bigpdf = pycpdflib.fromFile(args.file_to_split, '')
 outfilename = args.outfile
 
-#def pageRange(f, t):
-#""" Nuild a range from one page to another inclusive.
-#r = pycpdflib.pageRange(1,1)
 
-# selectPages(pdf, r):
-#weepdf = pycpdflib.selectPages(bigpdf, r)
-
-# pycpdflib.toFile(merged, 'merged.pdf', False, False)
-#pycpdflib.toFile(weepdf, '/Users/matthewtrimboli/drums-wee.pdf', False, False)
-
-with open('cute.yml') as file:
+with open(args.map) as file:
     try:
         chartmap = yaml.safe_load(file)   
-        #print(chartmap)
     except yaml.YAMLError as exc:
         print(exc)
 
@@ -43,18 +33,13 @@ for chair in chartmap['destinations']['chairs']:
     weepdf = ""
     r = ""
     print (chartmap['destinations']['chairs'][chair])
-    first = chartmap['destinations']['chairs'][chair]['first']
-    last = chartmap['destinations']['chairs'][chair]['last']
+    first = chartmap['destinations']['chairs'][chair][0]
+    last = chartmap['destinations']['chairs'][chair][1]
     r = pycpdflib.pageRange(first,last)
     weepdf = pycpdflib.selectPages(bigpdf, r)
-    pycpdflib.toFile(weepdf, '/Users/matthewtrimboli/' + chair + "-" + outfilename, False, False)
 
-
-#chartmap = yaml.load("cute.yml")
-
-#for i in `cat cute.map`
-#do 
-    #j=`echo $i | cut -f2 -d:`
-    #k=`echo $i | cut -f1 -d: `
-    #cpdf "Cute (Hefti).pdf" $j -o $k/422.pdf
-#done
+    if args.push:
+        print ("Pushing " + outfilename + " to " + SWING_SHIFT_PARTS_DIR + "/" + chair)
+        pycpdflib.toFile(weepdf, SWING_SHIFT_PARTS_DIR + "/" + chair + "/" + outfilename, False, False)
+    else:
+        print ("I would push " + outfilename + " to " + SWING_SHIFT_PARTS_DIR + "/" + chair)
